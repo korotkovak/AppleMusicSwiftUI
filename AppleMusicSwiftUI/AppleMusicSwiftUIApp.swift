60d81b2c8f1c34ed3ae38e5debddad13484d335b
korotkovak <kristinakorotkova059@gmail.com>
2023-03-29 22:55:08 +0800

    Initial Commit

---
+++ AppleMusicSwiftUI/AppleMusicSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicSwiftUIApp.swift
+//  AppleMusicSwiftUI
+//
+//  Created by Kristina Korotkova on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
